07231bf42d1cefbd9cd357b915c6c619b7f54c62
Benjamin Scholar <bbs27@case.edu>
2022-09-14 21:10:29 -0400

    minor changes

--- install.sh
+++ install.sh
@@ -1,9 +1,10 @@
-
+mkdir -p /var/lib/saab/
+chown saab:saab /var/lib/saab
 
 ln -s $(pwd)/saab.service /etc/systemd/user/saab.service 
-ls -s $(pwd)/saab.timer /etc/systemd/user/saab.timer 
-ls -s $(pwd)/saab_website_updates.py /usr/bin/saab_website_updates.py 
-ls -s $(pwd)/saab-config.yaml /etc/saab-config.yaml 
+ln -s $(pwd)/saab.timer /etc/systemd/user/saab.timer 
+ln -s $(pwd)/saab_website_updates.py /usr/bin/saab_website_updates.py 
+ln -s $(pwd)/saab-config.yaml /etc/saab-config.yaml 
 
 systemctl enable saab.service 
 systemctl start saab.service
